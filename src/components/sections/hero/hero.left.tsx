70cb84639ef4bc95b7c45349dc8e7ebf1dbdf2b1
Khang000 <khangquach026@gmail.com>
2025-06-30 17:18:34 +0700

    Update CV

--- src/components/sections/hero/hero.left.tsx
+++ src/components/sections/hero/hero.left.tsx
@@ -62,7 +62,7 @@ const HeroLeft = (props: IProps) => {
           btnIcons={<MdFileDownload />}
           onClick={() =>
             handleDownloadCV(
-              'https://drive.google.com/file/d/1trurLVxIsBIfxvrCIwOdgEwP4KA9DmNi/view?usp=sharing'
+              'https://drive.google.com/file/d/1DqF7XbZe6bSxNC7u4Gu_Up0UnQ2WIhGA/view?usp=sharing'
             )
           }
         />
